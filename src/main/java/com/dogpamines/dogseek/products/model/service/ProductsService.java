bcb8520d98ba836bbe8582f75198faa6afba0f72
kooyesung <jh8ckr@gmail.com>
2024-06-18 15:36:22 +0900

    selectAllProducts/selectAllProducts (#2)

--- src/main/java/com/dogpamines/dogseek/products/model/service/ProductsService.java
+++ src/main/java/com/dogpamines/dogseek/products/model/service/ProductsService.java
@@ -1,4 +1,21 @@
 package com.dogpamines.dogseek.products.model.service;
 
+import com.dogpamines.dogseek.products.model.dao.ProductsMapper;
+import com.dogpamines.dogseek.products.model.dto.ProductsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductsService {
+
+    private ProductsMapper productsMapper;
+
+    @Autowired
+    public ProductsService(ProductsMapper productsMapper) {this.productsMapper = productsMapper;}
+
+    public List<ProductsDTO> selectAllProducts() {
+        return productsMapper.selectAllProducts();
+    }
 }
